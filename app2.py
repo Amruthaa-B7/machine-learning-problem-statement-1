5ca70c86dcb8b9b72536f34d01f7408983045cfa
Ananda <135506157+Bottleneck44@users.noreply.github.com>
2024-11-09 06:41:49 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,1 +1,13 @@
+import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
+from langchain.memory import ConversationBufferMemory
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+import os
 
