fcb3b468227d70a8ee95f1915828afef8c0b7e62
Ananda <135506157+Bottleneck44@users.noreply.github.com>
2024-11-09 06:44:23 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -13,3 +13,25 @@ import os
 
 load_dotenv()
 os.environ["GOOGLE_API_KEY"]
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            page_text = page.extract_text()
+            if page_text:
+                text += page_text
+    return text
+  def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=8000,
+        chunk_overlap=1000,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+  def get_vectorstore(text_chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
